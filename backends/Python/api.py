08773e0b43059d086c0322ec4b166b6f3069742e
IITFernando <48291875+IITFernando@users.noreply.github.com>
2025-10-01 20:50:04 -0300

    Update api.py
    Correccion de api.py

--- backends/Python/api.py
+++ backends/Python/api.py
@@ -1,17 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
+from flask_cors import CORS
+from rutas import configurar_rutas
 
-app=Flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def index():
-    return render_template("index.html"),200
-
-@app.errorhandler(404)
-def not_found(error):
-    return render_template("error.html"),404
-
-
+# Configurar todas las rutas
+configurar_rutas(app)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=4000)
-    print ("Servidor a la espera de conexiones")
+    app.run(debug=True, port=5000)
